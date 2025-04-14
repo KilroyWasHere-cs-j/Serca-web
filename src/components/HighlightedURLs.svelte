5c397ca27cd2fbc14b9ea9a9c8152c6c751d465f
Gabriel Tower <gmtower1@proton.me>
2025-04-14 15:02:58 -0400

    reset

--- src/components/HighlightedURLs.svelte
+++
@@ -1,9 +0,0 @@
-<div class="flex justify-center">
-    <div class="flex-col border-2 p-2">
-        <ul class="list-disc list-inside text-gray-800 dark:text-white">
-            <li>Ah we are still building this website</li>
-            <li>We are getting close to begining THE CRAWL!!!!!!!!!!!!!!!!!!!!!!!!</li>
-            <li>Build cool stuff</li>
-        </ul>  
-    </div>  
-</div>
