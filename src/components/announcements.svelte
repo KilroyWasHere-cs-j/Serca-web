8fd235d3f6917cea4135165a316a696d88199fc3
Gabriel Tower <gmtower1@proton.me>
2025-04-14 14:35:35 -0400

    slowly building out this tool

---
+++ src/components/announcements.svelte
@@ -0,0 +1,9 @@
+<div class="flex justify-center border-red-500">
+    <div class="flex-col border-2 p-2">
+        <ul class="list-disc list-inside text-gray-800 dark:text-white">
+            <li>Ah we are still building this website</li>
+            <li>We are getting close to begining THE CRAWL!!!!!!!!!!!!!!!!!!!!!!!!</li>
+            <li>Build cool stuff</li>
+        </ul>  
+    </div>    
+</div>
