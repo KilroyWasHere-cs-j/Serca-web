441cdf9fe3f18e75af57fa813851e5b2228c646c
Gabriel Tower <gmtower1@proton.me>
2025-04-14 13:21:33 -0400

    Adding building page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,5 @@
     import { Button } from "$lib/components/ui/button";
 </script>
     
-<Button>Click me</Button>
+<h1>Serca: AI-powered media search engine</h1>
+<p>We still building this. I'm not a web dev so please bare with me...</p>
