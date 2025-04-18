1a63f76ae78b6d000f987010e72e6f1dbe8a099e
Gabriel Tower <gmtower1@proton.me>
2025-04-17 22:04:06 -0400

    Added a navbar

---
+++ serca-web/src/components/Navbar.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+</script>
+
+<div class="flex justify-center">
+    <div class="flex flex-col">
+        <a href="/" class="text-lg hover:text-purple-500 transition duration-300">Home</a>
+        <a href="/serca-search" class="text-lg hover:text-purple-500 transition duration-300">Search</a>
+        <a href="/database" class="text-lg hover:text-purple-500 transition duration-300">Database</a>
+        <a href="/vudoogroup" class="text-lg hover:text-purple-500 transition duration-300">Vudoo Group</a>
+    </div>
+</div>
