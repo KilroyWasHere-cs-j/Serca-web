ac63c69e3d0085832c530facd703a231faf01dc6
Gabriel <gmtower1@gmail.com>
2025-04-23 10:15:35 -0400

    Fixed issue

--- serca-web/src/routes/api/data/+server.ts
+++
@@ -1,9 +0,0 @@
-import { sql } from '../../../db.server';
-
-export async function load() {
-	const response = await sql`SELECT version()`;
-	const { version } = response[0];
-	return {
-		version
-	};
-}
