4dd4dd8009fa64a59571b9629b303e4cd9963f7d
Gabriel <gmtower1@gmail.com>
2025-04-24 13:55:27 -0400

    Adding AI interaction page

--- serca-web/src/routes/a/+page.svelte
+++ serca-web/src/routes/a/+page.svelte
@@ -1,1 +1,26 @@
-<p>A</p>
+<script>
+	import { onMount } from 'svelte';
+	export let data;
+
+	let prompt = '';
+	let response = '';
+
+	async function sendPrompt() {
+		const res = await fetch('/api/groq-chat', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ prompt })
+		});
+
+		const data = await res.json();
+		response = data.response;
+	}
+</script>
+
+<h1 class="flex justify-center">Testing page</h1>
+
+<textarea bind:value={prompt} placeholder="Ask something..."></textarea>
+<button on:click={sendPrompt}>Send</button>
+<pre>{response}</pre>
